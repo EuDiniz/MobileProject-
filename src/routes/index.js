9c4f3eff1ca821fa82d8b11824a55e9ca64560c0
Samuel Diniz <sdiniz00@gmail.com>
2022-10-01 12:46:14 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import { createNativeStackNavigator} from '@react-navigation/native-stack'
+
+import Welcome from '../pages/Welcome'
+import SignIn from '../pages/SignIn'
+/*import { Header } from 'reacte-native/Libraries/NewAppScreen';*/
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+            name="Welcome"
+            component={Welcome}
+            options={{ headerShown: false}}
+            />
+
+        <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false}}
+            />
+        </Stack.Navigator>
+
+    )
+}
